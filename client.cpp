c6d138a10099f84b20be68ec66aabd4ee0450e8b
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-03-09 19:04:00 +0100

    write query function for client that sends stuff to the server

--- client.cpp
+++ client.cpp
@@ -5,7 +5,74 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <assert.h>
+#include <cstring>
 using namespace std;
+
+const size_t k_max_msg = 4096;
+
+static int32_t read_full(int fd, char *buf, size_t n){
+    while (n > 0){
+        ssize_t rv = read(fd, buf, n);
+        if (rv <= 0){
+            return -1;
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t) rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+static int32_t write_all(int fd, const char *buf, size_t n){
+    while(n > 0){
+        ssize_t rv = write(fd, buf, n);
+        if(rv <= 0){
+            return -1;
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t) rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+static int32_t query(int fd, const char *text){
+    uint32_t len = (uint32_t) strlen(text);
+    if (len > k_max_msg){
+        return -1;
+    }
+    char wbuf[4 + k_max_msg];
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], text, len);
+    if(int32_t err = write_all(fd, wbuf, 4 + len)){
+        return err;
+    }
+    char rbuf[4 + k_max_msg + 1];
+    errno = 0;
+    int32_t err = read_full(fd, rbuf, 4);
+    if(err){
+        if(errno == 0){
+            perror("EOF");
+        } else {
+            perror("read");
+        }
+        return err;
+    }
+    memcpy(&len, rbuf, 4);
+    if(len > k_max_msg){
+        fprintf(stderr, "Message too long: %u \n", len);
+        return -1;
+    }
+    err = read_full(fd, &rbuf[4], len); // Request body
+    if(err){
+        perror("read() error");
+        return err;
+    }
+    rbuf[4 + len] = '\0';
+    printf("Server says: %s \n", &rbuf[4]);
+    return 0;
+}
 
 int main() {
     int fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -22,17 +89,5 @@ int main() {
         perror("connect");
         exit(1);
     }
-    char wbuf[] = "hello";
-    ssize_t x = write(fd, wbuf, sizeof(wbuf));
-    if(x < 0){
-        perror("write");
-        return 0;
-    }
-    char rbuf[64] = {};
-    ssize_t n = read(fd, rbuf, sizeof(rbuf) - 1);
-    if(n < 0){
-        perror("read");
-        return 0;
-    }
-    printf("Server says: %s \n", rbuf);
+    query(fd, "hello");
 }
