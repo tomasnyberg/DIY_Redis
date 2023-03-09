c6116cab1941b6d3310ed25b37414542aa88fab4
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-03-09 18:41:37 +0100

    add function for writing to socket as well in the same vein

--- server.cpp
+++ server.cpp
@@ -36,6 +36,20 @@ static int32_t read_full(int fd, char *buf, size_t n){
         n -= (size_t) rv;
         buf += rv;
     }
+    return 0;
+}
+
+static int32_t write_all(int fd, const char *buf, size_t n){
+    while(n > 0){
+        ssize_t rv = write(fd, buf, n);
+        if(rv <= 0){
+            return -1;
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t) rv;
+        buf += rv;
+    }
+    return 0;
 }
 
 int main() {
