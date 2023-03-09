6bdb92d4d69939e7c52e726e00d39b7af24a590a
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-03-09 17:59:31 +0100

    workign on accepting client connections on server

--- server.cpp
+++ server.cpp
@@ -1,9 +1,42 @@
 // #include  <bits/stdc++.h>
 #include <string>
 #include <iostream>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 using namespace std;
 
+static void do_something(int connfd){
+    char rbuf[64] = {};
+    ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
+}
+
 int main() {
-    cout << "Hello world\n";
+    int val = 1;
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = ntohs(1234);
+    addr.sin_addr.s_addr = ntohl(0);
+    int rv = bind(fd, (struct sockaddr *)&addr, sizeof(addr));
+    if (rv) {
+        perror("bind");
+        exit(1);
+    }
+    rv = listen(fd, SOMAXCONN);
+    if (rv) {
+        perror("listen()");
+        exit(1);
+    }
+    while(true){
+        struct sockaddr_in client_addr = {};
+        socklen_t socklen = sizeof(client_addr);
+        int connfd = accept(fd, (struct sockaddr *)&client_addr, &socklen);
+        if(connfd < 0){
+            continue;
+        }
+    }
 }
