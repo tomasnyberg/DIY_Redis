912b7d3e2d005f9efc388718af060a621256749e
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-03-26 14:10:38 +0200

    implemented sending requests from client, some error though

--- client.cpp
+++ client.cpp
@@ -8,65 +8,114 @@
 #include <cstring>
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include "util.h"
 using namespace std;
 
 const size_t k_max_msg = 4096;
 
-static int32_t query(int fd, const char *text) {
-    uint32_t len = (uint32_t)strlen(text);
+static void msg(const char *msg) {
+    fprintf(stderr, "%s\n", msg);
+}
+
+static void die(const char *msg) {
+    int err = errno;
+    fprintf(stderr, "[%d] %s\n", err, msg);
+    abort();
+}
+
+static int32_t send_req(int fd, const std::vector<std::string> &cmd) {
+    uint32_t len = 4;
+    for (const std::string &s : cmd) {
+        len += 4 + s.size();
+    }
     if (len > k_max_msg) {
         return -1;
     }
+
     char wbuf[4 + k_max_msg];
-    memcpy(wbuf, &len, 4);
-    memcpy(&wbuf[4], text, len);
-    if (int32_t err = write_all(fd, wbuf, 4 + len)) {
-        return err;
+    memcpy(&wbuf[0], &len, 4);  // assume little endian
+    uint32_t n = cmd.size();
+    memcpy(&wbuf[4], &n, 4);
+    size_t cur = 8;
+    for (const std::string &s : cmd) {
+        uint32_t p = (uint32_t)s.size();
+        memcpy(&wbuf[cur], &p, 4);
+        memcpy(&wbuf[cur + 4], s.data(), s.size());
+        cur += 4 + s.size();
     }
+    return write_all(fd, wbuf, 4 + len);
+}
+
+static int32_t read_res(int fd) {
+    // 4 bytes header
     char rbuf[4 + k_max_msg + 1];
     errno = 0;
     int32_t err = read_full(fd, rbuf, 4);
     if (err) {
         if (errno == 0) {
-            perror("EOF");
+            msg("EOF");
         } else {
-            perror("read");
+            msg("read() error");
         }
         return err;
     }
-    memcpy(&len, rbuf, 4);
+
+    uint32_t len = 0;
+    memcpy(&len, rbuf, 4);  // assume little endian
     if (len > k_max_msg) {
-        fprintf(stderr, "Message too long: %u \n", len);
+        msg("too long");
         return -1;
     }
-    err = read_full(fd, &rbuf[4], len); // Request body
+
+    // reply body
+    err = read_full(fd, &rbuf[4], len);
     if (err) {
-        perror("read() error");
+        msg("read() error");
         return err;
     }
-    rbuf[4 + len] = '\0';
-    printf("Server says: %s \n", &rbuf[4]);
+
+    // print the result
+    uint32_t rescode = 0;
+    if (len < 4) {
+        msg("bad response");
+        return -1;
+    }
+    memcpy(&rescode, &rbuf[4], 4);
+    printf("server says: [%u] %.*s\n", rescode, len - 4, &rbuf[8]);
     return 0;
 }
 
-int main() {
+int main(int argc, char **argv) {
     int fd = socket(AF_INET, SOCK_STREAM, 0);
     if (fd < 0) {
-        perror("socket");
-        exit(1);
+        die("socket()");
     }
+
     struct sockaddr_in addr = {};
     addr.sin_family = AF_INET;
     addr.sin_port = ntohs(1234);
-    addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK);
-    int rv = connect(fd, (struct sockaddr *)&addr, sizeof(addr));
+    addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK);  // 127.0.0.1
+    int rv = connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
     if (rv) {
-        perror("connect");
-        exit(1);
+        die("connect");
     }
-    query(fd, "hello1");
-    query(fd, "hello2");
-    query(fd, "hello3");
+
+    std::vector<std::string> cmd;
+    for (int i = 1; i < argc; ++i) {
+        cmd.push_back(argv[i]);
+    }
+    int32_t err = send_req(fd, cmd);
+    if (err) {
+        goto L_DONE;
+    }
+    err = read_res(fd);
+    if (err) {
+        goto L_DONE;
+    }
+
+L_DONE:
+    close(fd);
+    return 0;
 }
