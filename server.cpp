410f4d1246c98a3f74c8e779ebead377a6d8252c
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-04-07 18:25:23 +0200

    add lookup, inserting and popping, resizing logic

--- server.cpp
+++ server.cpp
@@ -42,6 +42,9 @@ struct Conn {
     uint8_t wbuf[4 + k_max_msg];
 };
 
+const size_t k_resizing_work = 128;
+const size_t k_max_load_factor = 8;
+
 struct HNode {
     HNode *next = NULL;
     uint64_t hcode = 0;
@@ -58,7 +61,7 @@ struct HTable {
         htab->size = 0;
     }
 
-    static void h_insert(HTable *htab, HNode *node){
+    static void h_insert(HTable *htab, HNode *node) {
         size_t pos = node->hcode & htab->mask;
         HNode *next = htab->tab[pos];
         node->next = next;
@@ -66,24 +69,102 @@ struct HTable {
         htab->size++;
     }
 
-    static HNode **h_lookup(HTable *htab, HNode *key, bool(*cmp) (HNode *, HNode *)){
-        if(!htab->tab) return NULL;
+    static HNode **h_lookup(HTable *htab, HNode *key, bool (*cmp)(HNode *, HNode *)) {
+        if (!htab->tab) return NULL;
         size_t pos = key->hcode & htab->mask;
         HNode **from = &htab->tab[pos];
-        while(*from){
-            if(cmp(*from, key)) return from;
+        while (*from) {
+            if (cmp(*from, key)) return from;
             from = &(*from)->next;
         }
         return NULL;
     }
 
-    static HNode *h_detach(HTable *htab, HNode **from){
+    static HNode *h_detach(HTable *htab, HNode **from) {
         HNode *node = *from;
         *from = node->next;
         htab->size--;
         return node;
     }
+
+    HNode *hm_lookup(HMap *hmap, HNode *key, bool (*cmp)(HNode *, HNode *)) {
+        hm_help_resizing(hmap);
+        HNode **from = h_lookup(&hmap->ht1, key, cmp);
+        if (!from) {
+            from = h_lookup(&hmap->ht2, key, cmp);
+        }
+        return from ? *from : NULL;
+    }
+
+    static void hm_help_resizing(HMap *hmap) {
+        if (hmap->ht2.tab == NULL) return;
+        size_t nwork = 0;
+        while (nwork < k_resizing_work && hmap->ht2.size > 0) {
+            HNode **from = &hmap->ht2.tab[hmap->resizing_pos];
+            if (!*from) {
+                hmap->resizing_pos++;
+                continue;
+            }
+            h_insert(&hmap->ht1, h_detach(&hmap->ht2, from));
+            nwork++;
+        }
+        if (hmap->ht2.size == 0) {
+            free(hmap->ht2.tab);
+            hmap->ht2 = HTable{};
+        }
+    }
+
+    void hm_insert(HMap *hmap, HNode *node) {
+        if (!hmap->ht1.tab) {
+            h_init(&hmap->ht1, 4);
+        }
+        h_insert(&hmap->ht1, node);
+        if (!hmap->ht2.tab) {
+            size_t load_factor = hmap->ht1.size / (hmap->ht1.mask + 1);
+            if (load_factor >= k_max_load_factor) {
+                hm_start_resizing(hmap);
+            }
+        }
+        hm_help_resizing(hmap);
+    }
+
+    static void hm_start_resizing(HMap *hmap) {
+        assert(hmap->ht2.tab == NULL);
+        // Bigger and swap
+        hmap->ht2 = hmap->ht1;
+        h_init(&hmap->ht2, hmap->ht1.mask * 2 + 1);
+        hmap->resizing_pos = 0;
+    }
+
+    HNode *hm_pop(HMap *hmap, HNode *key, bool (*cmp)(HNode *, HNode *)) {
+        hm_help_resizing(hmap);
+        HNode **from = h_lookup(&hmap->ht1, key, cmp);
+        if (from) {
+            return h_detach(&hmap->ht1, from);
+        }
+        from = h_lookup(&hmap->ht2, key, cmp);
+        if (from) {
+            return h_detach(&hmap->ht2, from);
+        }
+        return NULL;
+    }
 };
+
+struct HMap {
+    HTable ht1;
+    HTable ht2;
+    size_t resizing_pos = 0;
+};
+
+struct Entry {
+    struct HNode node;
+    string key;
+    string val;
+};
+
+static struct {
+    HMap db;
+} g_data;
 
 static void fd_set_nb(int fd) {
     errno = 0;
