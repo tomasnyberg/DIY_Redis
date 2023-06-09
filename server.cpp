c2d4f062bb8a67d4739d56eb4a92ef19eb65a985
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-06-09 10:58:53 +0200

    move enums to the util file

--- server.cpp
+++ server.cpp
@@ -25,31 +25,6 @@ using namespace std;
 const size_t k_max_msg = 4096;
 const size_t k_max_args = 1024;
 
-enum {
-    STATE_REQ = 0,
-    STATE_RES = 1,
-    STATE_END = 2,
-};
-
-enum {
-    RES_OK = 0,
-    RES_ERR = 1,
-    RES_NX = 2,
-};
-
-enum {
-    ERR_UNKNOWN = 1,
-    ERR_2BIG = 2,
-};
-
-enum {
-    SER_NIL = 0,
-    SER_ERR = 1,
-    SER_STR = 2,
-    SER_INT = 3,
-    SER_ARR = 4,
-};
-
 struct Conn {
     int fd = -1;
     uint32_t state = 0;
--- util.h
+++ util.h
@@ -11,6 +11,31 @@
 #include <iostream>
 #include <string>
 
+enum {
+    STATE_REQ = 0,
+    STATE_RES = 1,
+    STATE_END = 2,
+};
+
+enum {
+    RES_OK = 0,
+    RES_ERR = 1,
+    RES_NX = 2,
+};
+
+enum {
+    ERR_UNKNOWN = 1,
+    ERR_2BIG = 2,
+};
+
+enum {
+    SER_NIL = 0,
+    SER_ERR = 1,
+    SER_STR = 2,
+    SER_INT = 3,
+    SER_ARR = 4,
+};
+
 int32_t read_full(int fd, char *buf, size_t n);
 int32_t write_all(int fd, const char *buf, size_t n);
 
