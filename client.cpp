80cca67f21e6723ab9728068fe2f309327e1c441
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-06-09 11:16:11 +0200

    move on response up, fix printf error

--- client.cpp
+++ client.cpp
@@ -48,45 +48,6 @@ static int32_t send_req(int fd, const std::vector<std::string> &cmd) {
     return write_all(fd, wbuf, 4 + len);
 }
 
-static int32_t read_res(int fd) {
-    // 4 bytes header
-    char rbuf[4 + k_max_msg + 1];
-    errno = 0;
-    int32_t err = read_full(fd, rbuf, 4);
-    if (err) {
-        if (errno == 0) {
-            msg("EOF");
-        } else {
-            msg("read() error");
-        }
-        return err;
-    }
-
-    uint32_t len = 0;
-    memcpy(&len, rbuf, 4); // assume little endian
-    if (len > k_max_msg) {
-        msg("too long");
-        return -1;
-    }
-
-    // reply body
-    err = read_full(fd, &rbuf[4], len);
-    if (err) {
-        msg("read() error");
-        return err;
-    }
-
-    // print the result
-    uint32_t rescode = 0;
-    if (len < 4) {
-        msg("bad response");
-        return -1;
-    }
-    memcpy(&rescode, &rbuf[4], 4);
-    printf("server says: [%u] %.*s\n", rescode, len - 4, &rbuf[8]);
-    return 0;
-}
-
 static int32_t on_response(const uint8_t *data, size_t size){
     if(size < 1){
         msg("Bad response");
@@ -107,7 +68,7 @@ static int32_t on_response(const uint8_t *data, size_t size){
                     msg("Bad response");
                     return -1;
                 }
-                printf("(err) &d %.*s\n", code, len, &data[1 + 8]);
+                printf("(err) %d %.*s\n", code, len, &data[1 + 8]);
                 return 1 + 8 + len;
             }
         case SER_STR:
@@ -160,6 +121,44 @@ static int32_t on_response(const uint8_t *data, size_t size){
             msg("Bad response");
             return -1;
     }
+}
+
+
+static int32_t read_res(int fd) {
+    // 4 bytes header
+    char rbuf[4 + k_max_msg + 1];
+    errno = 0;
+    int32_t err = read_full(fd, rbuf, 4);
+    if (err) {
+        if (errno == 0) {
+            msg("EOF");
+        } else {
+            msg("read() error");
+        }
+        return err;
+    }
+
+    uint32_t len = 0;
+    memcpy(&len, rbuf, 4); // assume little endian
+    if (len > k_max_msg) {
+        msg("too long");
+        return -1;
+    }
+
+    // reply body
+    err = read_full(fd, &rbuf[4], len);
+    if (err) {
+        msg("read() error");
+        return err;
+    }
+
+    // print the result
+    int32_t rv = on_response((uint8_t *)&rbuf[4], len);
+    if (rv > 0 && (uint32_t)rv != len) {
+        msg("bad response");
+        rv = -1;
+    }
+    return rv;
 }
 
 int main(int argc, char **argv) {
