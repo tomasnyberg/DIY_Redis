194f56dc969f67b430f63a1d9d5bbd69576544ad
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-06-09 09:50:24 +0200

    add serialization enum

--- server.cpp
+++ server.cpp
@@ -37,6 +37,14 @@ enum {
     RES_NX = 2,
 };
 
+enum {
+    SER_NIL = 0,
+    SER_ERR = 1,
+    SER_STR = 2,
+    SER_INT = 3,
+    SER_ARR = 4,
+};
+
 struct Conn {
     int fd = -1;
     uint32_t state = 0;
