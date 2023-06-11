09d5dc8d4f12c3214c310b88bcb4869a810a4edb
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-06-11 10:42:05 +0200

    add rotation logic

--- avltree.cpp
+++ avltree.cpp
@@ -1,7 +1,8 @@
-#include "avltree.h" 
+#include "avltree.h"
+
 #include <assert.h>
-#include <stdlib.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 struct AVLNode {
     uint32_t depth = 0;
@@ -17,19 +18,83 @@ static void avl_init(AVLNode *node) {
     node->left = node->right = node->parent = NULL;
 }
 
-static uint32_t avl_depth(AVLNode *node){
+static uint32_t avl_depth(AVLNode *node) {
     return node ? node->depth : 0;
 }
 
-static uint32_t avl_cnt(AVLNode *node){
+static uint32_t avl_cnt(AVLNode *node) {
     return node ? node->cnt : 0;
 }
 
-static uint32_t max(uint32_t lhs, uint32_t rhs){
-    return lhs < rhs ? rhs:lhs;
+static uint32_t max(uint32_t lhs, uint32_t rhs) {
+    return lhs < rhs ? rhs : lhs;
 }
 
-static void avl_update(AVLNode *node){
+static void avl_update(AVLNode *node) {
     node->depth = max(avl_depth(node->left), avl_depth(node->right)) + 1;
     node->cnt = avl_cnt(node->left) + avl_cnt(node->right) + 1;
+}
+
+static AVLNode *rot_left(AVLNode *node) {
+    AVLNode *new_node = node->right;
+    if (new_node->left) {
+        new_node->left->parent = node;
+    }
+    node->right = new_node->left;
+    new_node->left = node;
+    new_node->parent = node->parent;
+    node->parent = new_node;
+    avl_update(node);
+    avl_update(new_node);
+    return new_node;
+}
+
+static AVLNode *rot_right(AVLNode *node) {
+    AVLNode *new_node = node->left;
+    if (new_node->right) {
+        new_node->right->parent = node;
+    }
+    node->left = new_node->right;
+    new_node->right = node;
+    new_node->parent = node->parent;
+    node->parent = new_node;
+    avl_update(node);
+    avl_update(new_node);
+    return new_node;
+}
+
+static AVLNode *avl_fix_left(AVLNode *root) {
+    if (avl_depth(root->left->left) < avl_depth(root->left->right)) {
+        root->left = rot_left(root->left);
+    }
+    return rot_right(root);
+}
+
+static AVLNode *avl_fix_right(AVLNode *root) {
+    if (avl_depth(root->right->right) < avl_depth(root->right->left)) {
+        root->right = rot_right(root->right);
+    }
+    return rot_left(root);
+}
+
+static AVLNode *avl_fix(AVLNode *node) {
+    while (true) {
+        avl_update(node);
+        uint32_t l = avl_depth(node->left);
+        uint32_t r = avl_depth(node->right);
+        AVLNode **from = NULL;
+        if (node->parent) {
+            from = (node->parent->left == node) ? &node->parent->left : &node->parent->right;
+        }
+        if (l == r + 2) {
+            node = avl_fix_left(node);
+        } else if (l + 2 == r) {
+            node = avl_fix_right(node);
+        }
+        if (!from){
+            return node;
+        }
+        *from = node;
+        node = node->parent;
+    }
 }
