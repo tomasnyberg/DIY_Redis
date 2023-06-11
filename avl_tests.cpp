6e312f633fbd4d1616c4a42014e6807be72f93e0
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-06-11 11:22:33 +0200

    add methods to be used for testing avl

--- avl_tests.cpp
+++ avl_tests.cpp
@@ -6,6 +6,8 @@
 
 #include "avltree.cpp" // lazy
 
+using namespace std;
+
 #define container_of(ptr, type, member) ({                  \
     const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
     (type *)( (char *)__mptr - offsetof(type, member) ); })
@@ -42,5 +44,64 @@ static void add(Container &c, uint32_t val) {
             break;
         }
         cur = *from;
+    }
+}
+
+static bool del(Container &c, uint32_t val){
+    AVLNode *cur = c.root;
+    while(cur) {
+        uint32_t node_val = container_of(cur, Data, node)->val;
+        if (val == node_val) {
+            break;
+        }
+        cur = val < node_val ? cur->left : cur->right;
+    }
+    if(!cur) return false;
+    c.root = avl_del(cur);
+    delete container_of(cur, Data, node);
+    return true;
+}
+
+static void avl_verify(AVLNode *parent, AVLNode *node){
+    if(!node) return;
+    assert(node->parent == parent);
+    avl_verify(node, node->left);
+    avl_verify(node, node->right);
+    assert(node->cnt == 1 + avl_cnt(node->left) + avl_cnt(node->right));
+    uint32_t l = avl_depth(node->left);
+    uint32_t r = avl_depth(node->right);
+    assert(l == r || l == r + 1 || l == r - 1);
+    assert(node->depth == 1 + max(l, r));
+    uint32_t val = container_of(node, Data, node)->val;
+    if(node->left){
+        assert(node->left->parent == node);
+        assert(container_of(node->left, Data, node)->val <= val);
+    }
+    if(node->right){
+        assert(node->right->parent == node);
+        assert(container_of(node->right, Data, node)->val >= val);
+    }
+}
+
+static void extract(AVLNode *node, multiset<uint32_t> &extracted){
+    if(!node) return;
+    extract(node->left, extracted);
+    extracted.insert(container_of(node, Data, node)->val);
+    extract(node->right, extracted);
+}
+
+static void container_verify(Container &c, const multiset<uint32_t> &ref){
+    avl_verify(NULL, c.root);
+    assert(avl_cnt(c.root) == ref.size());
+    multiset<uint32_t> extracted;
+    extract(c.root, extracted);
+    assert(extracted == ref);
+}
+
+static void dispose(Container &c){
+    while(c.root){
+        AVLNode *node = c.root;
+        c.root = avl_del(c.root);
+        delete container_of(node, Data, node);
     }
 }
