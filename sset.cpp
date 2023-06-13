2ccd3be487d5b9123584f3f52c5abed4e14e40a6
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-06-13 08:56:12 +0200

    run format, add comparator function

--- sset.cpp
+++ sset.cpp
@@ -1,4 +1,5 @@
 #include "sset.h"
+
 #include "avltree.h"
 #include "hashtable.h"
 #include "util.h"
@@ -17,13 +18,43 @@ struct ZNode {
 };
 
 static ZNode *znode_new(const char *name, size_t len, double score) {
-    ZNode *node = (ZNode *) malloc(sizeof(ZNode) + len);
+    ZNode *node = (ZNode *)malloc(sizeof(ZNode) + len);
     assert(node);
     avl_init(&node->tree);
     node->hmap.next = NULL;
-    node->hmap.hcode = str_hash((uint8_t *) name, len);
+    node->hmap.hcode = str_hash((uint8_t *)name, len);
     node->score = score;
     node->len = len;
     memcpy(&node->name[0], name, len);
     return node;
+}
+
+static void tree_add(ZSet *zset, ZNode *node) {
+    if (!zset->tree) {
+        zset->tree = &node->tree;
+        return;
+    }
+    AVLNode *cur = zset->tree;
+    while (true) {
+        AVLNode **from = zless(&node->tree, cur) ? &cur->left : &cur->right;
+        if (!*from) {
+            *from = &node->tree;
+            node->tree.parent = cur;
+            zset->tree = avl_fix(&node->tree);
+            break;
+        }
+        cur = *from;
+    }
+}
+
+static bool zless(AVLNode *lhs, double score, const char *name, size_t len) {
+    ZNode *zl = container_of(lhs, ZNode, tree);
+    if (zl->score < score) {
+        return zl->score < score;
+    }
+    int rv = memcmp(zl->name, name, zl->len < len ? zl->len : len);
+    if (rv != 0) {
+        return rv < 0;
+    }
+    return zl->len < len;
 }
