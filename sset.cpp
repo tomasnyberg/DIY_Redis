868e46dd73725a7ceed5659b97d2c2d9468e6fd9
tomasnyberg <tomas.nyberg7335@gmail.com>
2023-06-13 10:30:07 +0200

    fix header file for sset, add dispose and node del methods

--- sset.cpp
+++ sset.cpp
@@ -120,3 +120,21 @@ ZNode *zset_query(ZSet *zset, double score, const char *name, size_t len, int64_
     }
     return found ? container_of(found, ZNode, tree) : NULL;
 }
+
+void znode_del(ZNode *node) {
+    free(node);
+}
+
+static void tree_dispose(AVLNode *node) {
+    if (!node) {
+        return;
+    }
+    tree_dispose(node->left);
+    tree_dispose(node->right);
+    znode_del(container_of(node, ZNode, tree));
+}
+
+void zset_dispose(ZSet *zset) {
+    tree_dispose(zset->tree);
+    hm_destroy(&zset->hmap);
+}
--- sset.h
+++ sset.h
@@ -23,4 +23,14 @@ struct HKey {
     size_t len = 0;
 };
 
+
+bool zset_add(ZSet *zset, const char *name, size_t len, double score);
+ZNode *zset_lookup(ZSet *zset, const char *name, size_t len);
+ZNode *zset_pop(ZSet *zset, const char *name, size_t len);
+ZNode *zset_query(
+    ZSet *zset, double score, const char *name, size_t len, int64_t offset
+);
+void zset_dispose(ZSet *zset);
+void znode_del(ZNode *node);
+
 #endif
